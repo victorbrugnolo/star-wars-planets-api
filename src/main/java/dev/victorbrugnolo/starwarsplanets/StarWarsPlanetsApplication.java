a94aa45a21e317ffa2e53d18cda3f460d80fa91a
victorbrugnolo <brugnolovictor@gmail.com>
2020-08-18 20:55:24 -0300

    iniciando o projeto

---
+++ src/main/java/dev/victorbrugnolo/starwarsplanets/StarWarsPlanetsApplication.java
@@ -0,0 +1,13 @@
+package dev.victorbrugnolo.starwarsplanets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsPlanetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsPlanetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/victorbrugnolo/starwarsplanets/StarWarsPlanetsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.victorbrugnolo.starwarsplanets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsPlanetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
