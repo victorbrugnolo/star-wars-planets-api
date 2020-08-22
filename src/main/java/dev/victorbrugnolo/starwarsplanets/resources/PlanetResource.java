e05c788c5c50c86b7c8b7dab03fe604914ebebf8
victorbrugnolo <brugnolovictor@gmail.com>
2020-08-22 19:32:38 -0300

    definindo prefixo "/api" para todos os controllers

--- src/main/java/dev/victorbrugnolo/starwarsplanets/resources/PlanetResource.java
+++ src/main/java/dev/victorbrugnolo/starwarsplanets/resources/PlanetResource.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping("/api/planets")
+@RequestMapping("/planets")
 @RequiredArgsConstructor
 public class PlanetResource {
 
