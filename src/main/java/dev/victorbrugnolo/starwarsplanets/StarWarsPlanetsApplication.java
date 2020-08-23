647162fac436258e9e3e6e181e1d394349a3df54
victorbrugnolo <brugnolovictor@gmail.com>
2020-08-23 02:18:14 -0300

    adicionando swagger

--- src/main/java/dev/victorbrugnolo/starwarsplanets/StarWarsPlanetsApplication.java
+++ src/main/java/dev/victorbrugnolo/starwarsplanets/StarWarsPlanetsApplication.java
@@ -1,13 +1,22 @@
 package dev.victorbrugnolo.starwarsplanets;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StarWarsPlanetsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarWarsPlanetsApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Star Wars Planets API").version("1.0"));
 	}
 
 }
--- src/main/java/dev/victorbrugnolo/starwarsplanets/resources/PlanetResource.java
+++ src/main/java/dev/victorbrugnolo/starwarsplanets/resources/PlanetResource.java
@@ -4,6 +4,13 @@ import dev.victorbrugnolo.starwarsplanets.dtos.PlanetRequest;
 import dev.victorbrugnolo.starwarsplanets.dtos.PlanetResponse;
 import dev.victorbrugnolo.starwarsplanets.entities.Planet;
 import dev.victorbrugnolo.starwarsplanets.services.PlanetService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,14 +28,28 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping("/planets")
+@Tag(name = "Planets")
 @RequiredArgsConstructor
+@RequestMapping("/planets")
 public class PlanetResource {
 
   private final PlanetService planetService;
 
   @PostMapping
-  public ResponseEntity<Void> save(@Valid @RequestBody PlanetRequest planetRequest) {
+  @Operation(summary = "Create new planet")
+  @ApiResponses(
+      {
+          @ApiResponse(responseCode = "201", description = "Planet created", content = @Content),
+          @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
+          @ApiResponse(responseCode = "409", description = "Conflict", content = @Content),
+          @ApiResponse(responseCode = "500",
+              description = "Internal server error", content = @Content)
+      }
+  )
+  public ResponseEntity<Void> save(
+      @Valid
+      @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Planet request")
+      @RequestBody PlanetRequest planetRequest) {
     Planet planet = planetService.save(planetRequest);
     URI uri = ServletUriComponentsBuilder
         .fromCurrentRequest().path("/{id}").buildAndExpand(planet.getId())
@@ -37,27 +58,76 @@ public class PlanetResource {
   }
 
   @GetMapping
-  public ResponseEntity<Page<PlanetResponse>> getAllFromDatabase(Pageable pageable) {
+  @Operation(summary = "Obtain all planets from database")
+  @ApiResponses(
+      {
+          @ApiResponse(responseCode = "200", description = "Planets found", content = @Content(schema = @Schema(implementation = String.class))),
+          @ApiResponse(responseCode = "500",
+              description = "Internal server error", content = @Content)
+      }
+  )
+  public ResponseEntity<Page<PlanetResponse>> getAllFromDatabase(
+      @Parameter(hidden = true) Pageable pageable) {
     return ResponseEntity.ok(planetService.getAllFromDatabase(pageable));
   }
 
   @GetMapping("/swapi")
+  @Operation(summary = "Obtain all planets from SWAPI")
+  @ApiResponses(
+      {
+          @ApiResponse(responseCode = "200", description = "Planets found", content = @Content),
+          @ApiResponse(responseCode = "500",
+              description = "Internal server error", content = @Content),
+      }
+  )
   public ResponseEntity<Page<PlanetResponse>> getAllFromSwApi(
-      @RequestParam(required = false) Integer page) {
+      @Parameter(description = "Number of page") @RequestParam(required = false) Integer page) {
     return ResponseEntity.ok(planetService.getAllFromSwApi(page));
   }
 
   @GetMapping(params = "name")
-  public ResponseEntity<PlanetResponse> findByName(@RequestParam("name") String name) {
+  @Operation(summary = "Obtain planet by name")
+  @ApiResponses(
+      {
+          @ApiResponse(responseCode = "200", description = "Planet found",
+              content = @Content(schema = @Schema(implementation = PlanetResponse.class))),
+          @ApiResponse(responseCode = "404", description = "Planet not found", content = @Content),
+          @ApiResponse(responseCode = "500",
+              description = "Internal server error", content = @Content)
+      }
+  )
+  public ResponseEntity<PlanetResponse> findByName(
+      @Parameter(description = "Planet name", required = true) @RequestParam("name") String name) {
     return ResponseEntity.ok(planetService.findByName(name));
   }
 
   @GetMapping("{id}")
-  public ResponseEntity<PlanetResponse> findById(@PathVariable("id") String id) {
+  @Operation(summary = "Obtain planet by id")
+  @ApiResponses(
+      {
+          @ApiResponse(responseCode = "200",
+              description = "Planet found",
+              content = @Content(schema = @Schema(implementation = PlanetResponse.class))),
+          @ApiResponse(responseCode = "404", description = "Planet not found", content = @Content),
+          @ApiResponse(responseCode = "500",
+              description = "Internal server error", content = @Content)
+      }
+  )
+  public ResponseEntity<PlanetResponse> findById(
+      @Parameter(description = "Planet id", required = true) @PathVariable("id") String id) {
     return ResponseEntity.ok(planetService.findById(id));
   }
 
   @DeleteMapping("{id}")
+  @Operation(summary = "Delete planet")
+  @ApiResponses(
+      {
+          @ApiResponse(responseCode = "204", description = "Planet deleted", content = @Content),
+          @ApiResponse(responseCode = "404", description = "Planet not found", content = @Content),
+          @ApiResponse(responseCode = "500",
+              description = "Internal server error", content = @Content)
+      }
+  )
   public ResponseEntity<PlanetResponse> delete(@PathVariable("id") String id) {
     planetService.delete(id);
     return ResponseEntity.noContent().build();
